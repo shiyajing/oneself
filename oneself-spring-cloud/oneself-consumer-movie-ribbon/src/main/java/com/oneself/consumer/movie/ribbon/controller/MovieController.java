54259618f9c863e32a6c09c38a49460fdf2a10a7
shiyajing <shiyajing1314520@126.com>
2017-04-07 01:26:37 +0800

    添加电影微服务用Ribbon实现负载均衡

---
+++ oneself-spring-cloud/oneself-consumer-movie-ribbon/src/main/java/com/oneself/consumer/movie/ribbon/controller/MovieController.java
@@ -0,0 +1,35 @@
+package com.oneself.consumer.movie.ribbon.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.oneself.consumer.movie.ribbon.entity.UserInfo;
+
+@RestController
+public class MovieController {
+  @Autowired
+  private RestTemplate restTemplate;
+  @Autowired
+  private LoadBalancerClient loadBalancerClient;
+
+  @GetMapping("/movie/{username}")
+  public UserInfo findById(@PathVariable String username) {
+    return this.restTemplate.getForObject("http://oneself-provider-user/userinfo/simple/" + username, UserInfo.class);
+  }
+
+  @GetMapping("/test")
+  public String test() {
+    ServiceInstance serviceInstance = this.loadBalancerClient.choose("microservice-provider-user");
+    System.out.println("111" + ":" + serviceInstance.getServiceId() + ":" + serviceInstance.getHost() + ":" + serviceInstance.getPort());
+
+    ServiceInstance serviceInstance2 = this.loadBalancerClient.choose("microservice-provider-user2");
+    System.out.println("222" + ":" + serviceInstance2.getServiceId() + ":" + serviceInstance2.getHost() + ":" + serviceInstance2.getPort());
+
+    return "1";
+  }
+}
---
+++ oneself-spring-cloud/oneself-consumer-movie-ribbon/src/main/java/com/oneself/consumer/movie/ribbon/entity/UserInfo.java
@@ -0,0 +1,59 @@
+package com.oneself.consumer.movie.ribbon.entity;
+
+import java.io.Serializable;
+
+public class UserInfo implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 204280193416956395L;
+
+	private String userId;
+	private String userName;
+	private String userPassword;
+	private String delFlag;
+
+	public UserInfo() {
+	}
+
+	public UserInfo(String userId, String userName, String userPassword, String delFlag) {
+		super();
+		this.userId = userId;
+		this.userName = userName;
+		this.userPassword = userPassword;
+		this.delFlag = delFlag;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserPassword() {
+		return userPassword;
+	}
+
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+
+	public String getDelFlag() {
+		return delFlag;
+	}
+
+	public void setDelFlag(String delFlag) {
+		this.delFlag = delFlag;
+	}
+}
