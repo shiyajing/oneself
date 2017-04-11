56529aabcb0783a2e8a81097a4d8a8b411cd58c3
shiyajing <shiyajing1314520@126.com>
2017-04-11 11:36:15 +0800

    添加配置

--- oneself-spring-cloud/oneself-api-gateway/src/main/java/com/oneself/cloud/api/gateway/ZuulApiGatewayApplication.java
+++ oneself-spring-cloud/oneself-api-gateway/src/main/java/com/oneself/cloud/api/gateway/ZuulApiGatewayApplication.java
@@ -1,10 +1,22 @@
 package com.oneself.cloud.api.gateway;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
 /**
+ * 使用@EnableZuulProxy注解激活zuul
+ * 跟进该注解可以看到该注解整合了@EnableCircuitBreaker、@EnableDiscoveryClient，是个组合注解，目的是简化配置。
+ * 
  * @author shiyajing
  * @E-mail 345129564@qq.com
  * @version 2017年4月11日上午12:22:24
  */
+@SpringBootApplication
+@EnableZuulProxy
 public class ZuulApiGatewayApplication {
 
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulApiGatewayApplication.class, args);
+	}
 }
